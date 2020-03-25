31be1a1d2f4c75f018e188b5befdcddfd73d59d6
Carlos Esteban Guerrero Robles <carlos.guerrero@utec.edu.pe>
2020-03-25 13:06:39 -0500

    Read document type and number

--- src/app/src/main/java/com/example/covid19_utec/MainActivity.java
+++ src/app/src/main/java/com/example/covid19_utec/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.covid19_utec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     //
@@ -14,11 +19,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final EditText editText = (EditText) findViewById(R.id.nDocumento);
+        final TelephonyManager telephonyManager = (TelephonyManager) getBaseContext().getSystemService(Context.TELEPHONY_SERVICE);
         ArrayAdapter<CharSequence> myadapter=  ArrayAdapter.createFromResource(this,R.array.opciones_login,R.layout.spinner_item);
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(myadapter);
         spinner.setOnItemSelectedListener(this);
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String type = spinner.getSelectedItem().toString();
+                String document = editText.getText().toString();
+                String phone = telephonyManager.getDeviceId();
+                Toast.makeText(getApplicationContext(), "Tipo de documento: " + type + "\nNúmero de documento: " + document,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -28,6 +45,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void login(View view) {
 
     }
 }
